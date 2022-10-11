472e8032dbef688f61d77dd32df0bfe46f8d0d3f
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-10-12 00:54:59 +0330

    Rewrite Oprations component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -8,16 +8,16 @@ export default function Buttons(){
         <div className="buttons-container">
             <div className="number-oprations-container">
                 <div className="numbers-container">
-                    <button id="number-seven" onClick={Oprations.pushNumber(7)}>7</button>
-                    <button id="number-eight" onClick={Oprations.pushNumber(8)}>8</button>
-                    <button id="number-nine" onClick={Oprations.pushNumber(9)}>9</button>
-                    <button id="number-four" onClick={Oprations.pushNumber(4)}>4</button>
-                    <button id="number-five" onClick={Oprations.pushNumber(5)}>5</button>
-                    <button id="number-six" onClick={Oprations.pushNumber(6)}>6</button>
-                    <button id="number-one" onClick={Oprations.pushNumber(1)}>1</button>
-                    <button id="number-two" onClick={Oprations.pushNumber(2)}>2</button>
-                    <button id="number-three" onClick={Oprations.pushNumber(3)}>3</button>
-                    <button id="number-zero" onClick={Oprations.pushNumber(0)}>0</button>
+                    <button id="number-seven" onClick={function(){Oprations.pushNumber(7)}}>7</button>
+                    <button id="number-eight" onClick={function(){Oprations.pushNumber(8)}}>8</button>
+                    <button id="number-nine" onClick={function(){Oprations.pushNumber(9)}}>9</button>
+                    <button id="number-four" onClick={function(){Oprations.pushNumber(4)}}>4</button>
+                    <button id="number-five" onClick={function(){Oprations.pushNumber(5)}}>5</button>
+                    <button id="number-six" onClick={function(){Oprations.pushNumber(6)}}>6</button>
+                    <button id="number-one" onClick={function(){Oprations.pushNumber(1)}}>1</button>
+                    <button id="number-two" onClick={function(){Oprations.pushNumber(2)}}>2</button>
+                    <button id="number-three" onClick={function(){Oprations.pushNumber(3)}}>3</button>
+                    <button id="number-zero" onClick={function(){Oprations.pushNumber(0)}}>0</button>
                     <button id="point">.</button>
                     <button id="del">&#8592;</button>
                 </div>
--- src/components/Oprations.jsx
+++ src/components/Oprations.jsx
@@ -1,20 +1,39 @@
-let Oprations;
-export default Oprations = {
-    "stack": [],
-    "showMonitor": "",
-    "toShow": function (){
-        let showStr = " ";
-        for (let i in this.stack){
-            showStr += this.stack[i].toString();
+var stack = [];
+
+function PushNumber(number){
+    if (typeof stack[length -1] == 'number'){
+        stack.push(number);
+    } else {
+        alert('Input opration between numbers !!!');
+    }
+}
+
+function PushOpration(opration){
+    if (typeof stack[length -1] == 'string'){
+        stack.push(opration);
+    } else {
+        alert('First input Operand !!!')
+    }
+}
+
+function compute(){
+    let answer = 0;
+    let popStack = stack.pop();
+    if (typeof popStack == 'number'){
+        let opration = stack.pop()
+        let firstOpe = stack.pop()
+        if (opration == '+'){
+            answer += popStack + firstOpe;
+        } else if (opration == '-'){
+            answer += firstOpe - popStack 
+        } else if (opration == '*'){
+            answer += firstOpe * popStack
+        } else if (opration == '/'){
+            answer += firstOpe / popStack
+        } else{
+            alert('Input is wrong !!!!')
         }
-        this.showMonitor = showStr;
-    },
-    "pushNumber": function (number){
-        this.stack.push(number);
-        this.toShow();
-        // alert('he')
-    },
-    "pushOpration":function (opration){
-        pass
+    } else {
+        alert('input latest operand !!!');
     }
 }
